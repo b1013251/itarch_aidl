ed6d7afcd4bbcd9178ed978c27157765c1eebbea
b1013251 <b1013251@gmail.com>
2016-11-07 15:23:50 +0900

    first commit

---
+++ app/src/main/java/b1013251/itarch/com/aidlpractice/MainActivity.java
@@ -0,0 +1,13 @@
+package b1013251.itarch.com.aidlpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
